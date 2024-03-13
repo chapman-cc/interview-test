8a335cf77fa961bb0ac5625d51ef7ff7786c1a80
ChapmanCheng <chapboy1989@gmail.com>
2024-03-13 23:15:43 +0000

    fix(profile): redirect user to login in token in not available

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,39 @@
 "use client";
 
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import Field from "./Field";
 import { useLocalStorage } from "usehooks-ts";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Profile() {
   const [token] = useLocalStorage("TOKEN", null);
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  const router = useRouter();
 
   async function fetchUserProfile() {
-    const url = "/api/profile";
-    const method = "POST";
-    const body = JSON.stringify({ token });
-    const res = await fetch(url, { method, body });
-    const { name, email } = await res.json();
-    setName(name);
-    setEmail(email);
+    try {
+      const url = "/api/profile";
+      const method = "POST";
+      const body = JSON.stringify({ token });
+      const res = await fetch(url, { method, body });
+      if (!res.ok) {
+        throw new Error("Fetch profile error");
+      }
+
+      const { name, email } = await res.json();
+      setName(name);
+      setEmail(email);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   useEffect(() => {
+    if (token === null) {
+      router.push("/");
+    }
     fetchUserProfile();
   }, []);
 

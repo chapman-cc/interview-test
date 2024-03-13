49e8a41238ccb1427688459859d52210afc47faa
ChapmanCheng <chapboy1989@gmail.com>
2024-03-13 22:47:30 +0000

    feat(api/profile): complete api profile

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,1 +1,14 @@
-/** @TODO */
+
+const userProfile: UserProfile = {
+  name: "John Doe",
+  email: "john@example.com",
+};
+
+export async function POST(request: Request) {
+  const { token } = await request.json();
+  if (token === "123") {
+    return Response.json(userProfile);
+  } else {
+    return Response.error();
+  }
+}
